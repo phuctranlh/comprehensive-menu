250c1a97546bf8286f869824e820a8ff26ad6a72
phuctranlh <phuctran.lh@gmail.com>
2020-10-22 00:59:51 +1000

    Add child menus

--- src/components/menu/MenuComp.tsx
+++ src/components/menu/MenuComp.tsx
@@ -5,24 +5,63 @@ function MenuComp(props) {
         <div className="nav-links">
             <ul>
                 <li className="first-nav"><a href="/">Home </a></li>
-                <li className="first-nav"><a href="/orders">Programming </a></li>
                 <li className="first-nav menu-has-sub">
-                    <input type="checkbox" id="menu-check0.2" />
-                    <a href="/">Travel
-                        <label title="toggle menu" htmlFor="menu-check0.2">
+                    <input type="checkbox" id="menu-check1.2" />
+                    <a href="/programming">Programming
+                        <label title="toggle menu" htmlFor="menu-check1.2">
                             <i className="fa fa-caret-down" aria-hidden="true"></i>
                         </label>
                     </a>
+                    <div className="nav-dropdown expanded">
+                        <ul className="sub-menu">
+                            <li className="child-nav"><a href="/programming/front-end"> Front-end </a></li>
+                            <li className="child-nav menu-has-sub">
+                                <input type="checkbox" id="menu-check2.2" />
+                                <a href="/programming/back-end"> Back-end
+                                    <label title="toggle menu" htmlFor="menu-check2.2">
+                                        <i className="fa fa-caret-down" aria-hidden="true"></i>
+                                    </label>
+                                </a>
+                                <div className="nav-dropdown child-dropdown expanded">
+                                    <ul className="sub-menu">
+                                        <li className="child-nav"><a href="/programming/back-end/java"> Java </a></li>
+                                        <li className="child-nav"><a href="/programming/back-end/spring"> Spring </a></li>
+                                    </ul>
+                                </div></li>
+                        </ul>
+                    </div>
                 </li>
-                <li className="first-nav"><a href="/reports">Life </a></li>
-                <li className="first-nav menu-has-sub">
-                    <input type="checkbox" id="menu-check0.4" />
-                    <a href="/">About
-                        <label title="toggle menu" htmlFor="menu-check0.4">
+                <li className="first-nav"><a href="/travel">Travel </a></li>
+                <li className="first-nav">
+                    <input type="checkbox" id="menu-check1.4" />
+                    <a href="/life">Life
+                        <label title="toggle menu" htmlFor="menu-check1.4">
                             <i className="fa fa-caret-down" aria-hidden="true"></i>
                         </label>
                     </a>
+                    <div className="nav-dropdown expanded">
+                        <ul className="sub-menu">
+                            <li className="child-nav menu-has-sub">
+                                <input type="checkbox" id="menu-check2.4" />
+                                <a href="/life/cooking"> Cooking
+                                    <label title="toggle menu" htmlFor="menu-check2.4">
+                                        <i className="fa fa-caret-down" aria-hidden="true"></i>
+                                    </label>
+                                </a>
+                                <div className="nav-dropdown child-dropdown expanded">
+                                    <ul className="sub-menu">
+                                        <li className="child-nav"><a href="/life/cooking/food"> Food </a></li>
+                                        <li className="child-nav"><a href="/life/cooking/drink"> Drink </a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li className="child-nav"><a href="/life/tips"> Tips </a></li>
+                            <li className="child-nav"><a href="/life/funny"> Funny </a></li>
+                            <li className="child-nav"><a href="/life/stories"> Stories </a></li>
+                        </ul>
+                    </div>
                 </li>
+                <li className="first-nav menu-has-sub"><a href="/">About </a></li>
             </ul>
         </div>
 );
