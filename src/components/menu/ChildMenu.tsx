54e7e80d0382d4bef34fb2902cb3400e7c9d7829
phuctranlh <phuctran.lh@gmail.com>
2020-10-23 23:56:54 +1000

    Show hide menu using react state
    OnEnter and onLeave function

--- src/components/menu/ChildMenu.tsx
+++ src/components/menu/ChildMenu.tsx
@@ -7,7 +7,25 @@ function ChildMenu(props) {
     // Get menu info from items
     const {gen_key, name, link, level, items = []} = props;
 
+    //Check if the menu has children
     const isExpandable = items && items.length > 0;
+
+    //Handle dropdown using react state
+    const [dropdown, setDropdown] = useState(false);
+
+    //Show menu when hovering
+    const hoverMenu = (evt, lk) => {
+        if(window.innerWidth >= 960) {
+            setDropdown(true);
+        }
+    }
+
+    //Hide menu when leaving
+    const leaveMenu = (evt, lk) => {
+        if(window.innerWidth >= 960) {
+            setDropdown(false);
+        }
+    }
 
     //Handle menu without link
     let n_link = link;
@@ -29,9 +47,12 @@ function ChildMenu(props) {
 
     return (
         <>
-            <li key={gen_key} className={classN}>
-
-                {isExpandable? <input type="checkbox" id={'menu-check' + level + '.' + gen_key}/> : null}
+            <li key={gen_key} className={classN}
+                onMouseEnter={(event) => hoverMenu(event, n_link)}
+                onMouseLeave={(event) => leaveMenu(event, n_link)}>
+                {isExpandable? <input type="checkbox" id={'menu-check' + level + '.' + gen_key}>
+                                </input> : null
+                }
                 <Link to={n_link}>
                     {name}
                     {isExpandable? <label title="toggle menu" htmlFor={'menu-check' + level + '.' + gen_key}>
@@ -41,10 +62,11 @@ function ChildMenu(props) {
 
                 </Link>
 
-                <BoundChildMenu sub_items={items}
+                {dropdown && <BoundChildMenu sub_items={items}
                                 level={level}
                                 expand={isExpandable}>
-                </BoundChildMenu>
+                    </BoundChildMenu>
+                }
 
             </li>
 
